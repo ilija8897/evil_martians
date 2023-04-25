5b5291dfeba5be1ad24750f9614add44ff2604e5
Ilya Nikiforov <ilija8897@yandex.ru>
2023-04-25 15:52:41 +0300

    add eslint + prettier + husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import React from 'react';
 import './App.css';
 
-export const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Главная
-      </header>
+export const App = () => (
+    <div className='App'>
+        <header className='App-header'>Главная</header>
     </div>
-  );
-}
+);
